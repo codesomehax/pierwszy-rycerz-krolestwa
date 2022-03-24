e3369dddde4235a9441773e118642af24cfd7896
codesomehax <markiewicz.darek.dm@gmail.com>
2022-03-24 20:15:36 +0100

    Player can fully animate (except for diagonal moves) and move, but after movement there is a gliding effect

--- Assets/Scripts/HumanMovement.cs
+++ Assets/Scripts/HumanMovement.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HumanMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class HumanMovement : MonoBehaviour {
+
+    private Animator animator;
+    private CharacterController characterController;
+    
+    private float horizontalMovement = 0f;
+    private float verticalMovement = 0f;
+    private bool intendsToRun = false;
+
+    public float walkVelocity = 2f;
+    public float runVelocity = 5f;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+   
+    void Update() {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        intendsToRun = Input.GetKey(KeyCode.LeftShift);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        Vector3 direction = new Vector3(horizontalMovement, 0, verticalMovement);
+        direction.Normalize();
+
+        float velocity = (intendsToRun) ? runVelocity : walkVelocity;
+        Vector3 movement = transform.TransformDirection(direction) * velocity;
+
+        characterController.SimpleMove(movement);
+
+        float factor = (intendsToRun) ? 2f : 1f;
+        animator.SetFloat("VelocityX", horizontalMovement * factor, 0.1f, Time.deltaTime);
+        animator.SetFloat("VelocityZ", verticalMovement * factor, 0.1f, Time.deltaTime);
     }
 }
