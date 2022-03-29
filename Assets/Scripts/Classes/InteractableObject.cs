de080d23635617c1fc5f3fbca6076da72065e35d
codesomehax <markiewicz.darek.dm@gmail.com>
2022-03-29 18:34:57 +0200

    Classes final sketch done, ready for review

--- Assets/Scripts/Classes/InteractableObject.cs
+++ Assets/Scripts/Classes/InteractableObject.cs
@@ -9,4 +9,11 @@ using UnityEngine;
 public class InteractableObject : MonoBehaviour
 {
     public string Name;
+
+    /**
+    * * Important! ID is not an identifier of a unique instance. There can be 2 objects having the same ID.
+    * * The reason is to throw everything of the same kind into the same bag; imagine 2 goblins that have
+    * * the same ID, which purpose is to make it easier for for example kill quests.
+    */
+    public int ID;
 }
--- Assets/Scripts/Classes/NPC.cs
+++ Assets/Scripts/Classes/NPC.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /**
-* There isn't a class called Monster or anything like that; we can consider something a monster
+* * There isn't a class called Monster or anything like that
+* We can consider something a monster
 * maybe if it wants to attack us, which we can detect using IsAggresiveTowardsPlayer() method
 * which is based on the reputation of the player.
+*
+* * If a monster has special traits, create new class inheriting NPC
 */
 public class NPC : Entity
 {
