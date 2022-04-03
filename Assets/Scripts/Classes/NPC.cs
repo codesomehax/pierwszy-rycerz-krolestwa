4087136f2b8b6cbc1b9b59ab4ca25ac08b7ac93c
codesomehax <markiewicz.darek.dm@gmail.com>
2022-04-03 17:29:28 +0200

    AnimatorOverrideController works for 1 object, lets test it for multiple

--- Assets/Scripts/Classes/NPC.cs
+++ Assets/Scripts/Classes/NPC.cs
@@ -15,15 +15,15 @@ public class NPC : Entity
     public Alliance Alignment;
     public float AggressivenessBorder; // aggressive if reputation is lower than this value
     public float AggressivenessDistance; // distance from where the NPC will start chasing the player (if aggressive)
+    public AnimationClip IdleAnimation;    
+    public AnimationClip WalkingAnimation;
+    public AnimationClip BasicAttackAnimation;
 
 
 
     protected Animator _animator;
     protected Player _player;
-
-
-
-    private Dictionary<NPCAnimationState, string> _animationStates;
+    protected AnimatorOverrideController _animatorOverrideController;
 
 
 
@@ -37,9 +37,35 @@ public class NPC : Entity
         _animator = GetComponent<Animator>();
         _player = FindObjectOfType<Player>();
 
-        _animationStates.Add(NPCAnimationState.Idle, "Idle");
-        _animationStates.Add(NPCAnimationState.Walking, "Walking");
-        _animationStates.Add(NPCAnimationState.BasicAttack, "Basic Attack");
+        _animatorOverrideController = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+        _animator.runtimeAnimatorController = _animatorOverrideController;
+
+        AnimationClipOverrides overrides = new AnimationClipOverrides(_animatorOverrideController.overridesCount);
+        _animatorOverrideController.GetOverrides(overrides);
+
+        overrides["Idle"] = IdleAnimation;
+        overrides["Walking"] = WalkingAnimation;
+        overrides["Basic Attack"] = BasicAttackAnimation;
+
+        _animatorOverrideController.ApplyOverrides(overrides);
+
+
+
     }
 }
 
+public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
+{
+    public AnimationClipOverrides(int capacity) : base(capacity) {}
+
+    public AnimationClip this[string name]
+    {
+        get { return this.Find(x => x.Key.name.Equals(name)).Value; }
+        set
+        {
+            int index = this.FindIndex(x => x.Key.name.Equals(name));
+            if (index != -1)
+                this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
+        }
+    }
+}
