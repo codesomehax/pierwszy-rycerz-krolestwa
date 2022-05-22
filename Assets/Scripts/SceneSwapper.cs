6d19ea46927700a96b2f44c3c30109b64bbe32e2
codesomehax <markiewicz.darek.dm@gmail.com>
2022-05-22 22:52:19 +0200

    Implemented scene persistance, now the player has to update according to his state in the previous scene

--- Assets/Scripts/SceneSwapper.cs
+++ Assets/Scripts/SceneSwapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using SaveIsEasy;
 
 public class SceneSwapper : MonoBehaviour
 {
@@ -13,12 +14,19 @@ public class SceneSwapper : MonoBehaviour
     private void Awake()
     {
         _canEnter = false;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.F) && _canEnter)
         {
+            SaveIsEasyAPI.SaveAll(SceneManager.GetActiveScene().name + ".game");
             SceneManager.LoadScene(GotoScene.name);
         }
     }
@@ -33,5 +41,13 @@ public class SceneSwapper : MonoBehaviour
     {
         if (other.gameObject.name == "Player")
             _canEnter = false;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (SaveIsEasyAPI.FileExists(scene.name + ".game"))
+        {
+            SaveIsEasyAPI.LoadAll(scene.name + ".game");
+        }
     }
 }
